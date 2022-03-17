d1bdf28dd4d9aa6a80f0d022cbb086c6f9afe0be
jfprgin <jprgin00@fesb.hr>
2022-03-17 17:40:33 +0100

    Add project files.

---
+++ PIS_Vj1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PIS_Vj1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
